0bfa6d80d8f071ec35ed08ae16c14092cb6662a0
Roman Petryaev <petryaev.roman1@gmail dot com>
2021-01-31 21:32:36 +0300

    It was developing but not checked a class "alien_snake"

--- alien_snake.cpp
+++ alien_snake.cpp
@@ -3,31 +3,80 @@
 namespace Game_elements
 {
     namespace Network_elements
-    {     
-        enum State{
-            alive = 1,
-            dead = 0
-        };
+    {
+        const int buffer_size = 6;
+        
         class alien_snake
-        {        
-            const int buffer_size = 6;
-            
+        {            
             struct Star{
                 int x, y;
                 Star * next;
             };
+            WINDOW * win;
+            Star * star;
             int buff[buffer_size];
             int points_per_game;
-            State state;
             
         public:
+            alien_snake(WINDOW * win, int x, int y);
+            ~alien_snake();
             
             void getData(int * data);
+            void move();
+            void checkPoints();
+            void changeLocation(Star * star, int x, int y);
+            void hideSnake();
         };
         
         void alien_snake::getData(int * data){
             for(int i = 0; i < buffer_size; ++i)
                 buff[i] = data[i];
+        }
+        
+        void alien_snake::move()
+        {
+            hideSnake();
+            changeLocation(star, buff[0], buff[1]);
+        }
+        
+        void alien_snake::changeLocation(Star * star, int x, int y)
+        {
+            if(!star)
+                return;
+            changeLocation(star->next, star->x, star->y);
+            star->x = x;
+            star->y = y;
+            wmove(win, star->y, star->x);
+            waddch(win, '*');
+        }
+        
+        void alien_snake::hideSnake()
+        {
+            Star * tmp = star;
+            while(tmp->next)
+                tmp = tmp->next;
+            wmove(win, tmp->y, tmp->x);
+            waddch(win, ' ');
+        }
+        
+        alien_snake::alien_snake(WINDOW * win, int x, int y) : points_per_game(0)
+        {
+            this->win = win;
+            star = new Star;
+            star->x = x;
+            star->y = y;
+            wmove(win, star->y, star->x);
+            waddch(win, '*');
+        }
+        
+        alien_snake::~alien_snake()
+        {
+            Star * tmp;
+            while(star){
+                tmp = star;
+                star = star->next;
+                delete tmp;
+            }
         }
         
     }
